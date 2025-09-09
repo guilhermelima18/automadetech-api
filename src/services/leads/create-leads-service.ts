dc211e63372c61d9e02859d2691208420c4784e1
guilhermelima18 <guilhermelima18@hotmail.com>
2025-09-08 22:22:51 -0300

    feat: add disparo de e-mail na criação do lead

--- src/services/leads/create-leads-service.ts
+++ src/services/leads/create-leads-service.ts
@@ -1,4 +1,6 @@
 import admin from "firebase-admin";
+import { leadEmail } from "../../templates/lead-email";
+import { transporter } from "../emails/transporter";
 
 interface ICreateLeadsService {
   email: string;
@@ -23,6 +25,22 @@ class CreateLeadsService {
     if (!docRef) {
       throw new Error("Não foi possível criar o lead!");
     }
+
+    const html = leadEmail
+      .replace(/{{email}}/g, email)
+      .replace(/{{logo_url}}/g, "https://sua-cdn.com/logo.png")
+      .replace(/{{brand_url}}/g, "https://automadetech.com")
+      .replace(/{{cta_url}}/g, "https://automadetech.com/demo")
+      .replace(/{{cta_label}}/g, "Agendar demonstração")
+      .replace(/{{unsubscribe_url}}/g, "https://automadetech.com/unsubscribe");
+
+    await transporter.sendMail({
+      from: `"Automadetech" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Pré lançamento da Automadetech",
+      text: "Olá! Este é um e-mail de pré lançamento da plataforma Automadetech.",
+      html,
+    });
 
     return { id: docRef.id, message: "Lead criado com sucesso!" };
   }
